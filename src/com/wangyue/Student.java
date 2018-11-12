57e485ac5b4e644692a0546ce2857fadfbdb8d4a
wangyue <18845117946@163.com>
2018-11-12 13:06:12 +0800

    hello

---
+++ src/com/wangyue/Student.java
@@ -0,0 +1,9 @@
+package com.wangyue;
+
+import java.util.MissingFormatArgumentException;
+
+public class Student {
+   public static void main(String[] args) {
+	System.out.println("hello world!");
+}
+}   
