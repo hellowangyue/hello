49b501a1363cde3a9d0fa640a6921b1be43502e7
hellozhanglixue <15776682236@163.com>
2018-11-13 12:55:53 +0800

    wangyue_update08

--- src/com/wangyue/Student.java
+++ src/com/wangyue/Student.java
@@ -8,5 +8,6 @@ public class Student {
 	System.out.println("hello world! zhanglixue");
 	System.out.println("hello world! zhanglixue");
 	System.out.println("hello world! zhanglixue");
+	System.out.println("hello world! wangyue");
 }
 }   
