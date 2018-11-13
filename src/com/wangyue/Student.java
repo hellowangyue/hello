994e99c8441fd72e01d75733ddb904925231df3b
hellozhanglixue <15776682236@163.com>
2018-11-13 12:50:00 +0800

    zhanglixue_update07

--- src/com/wangyue/Student.java
+++ src/com/wangyue/Student.java
@@ -5,5 +5,8 @@ import java.util.MissingFormatArgumentException;
 public class Student {
    public static void main(String[] args) {
 	System.out.println("hello world! wangyue");
+	System.out.println("hello world! zhanglixue");
+	System.out.println("hello world! zhanglixue");
+	System.out.println("hello world! zhanglixue");
 }
 }   
