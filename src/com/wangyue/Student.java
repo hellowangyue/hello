4e8bfa61f7e63dd5b89d8c993a9ab86611ab9303
wangyue <18845117946@163.com>
2018-11-13 09:01:03 +0800

    update 04

--- src/com/wangyue/Student.java
+++ src/com/wangyue/Student.java
@@ -5,9 +5,5 @@ import java.util.MissingFormatArgumentException;
 public class Student {
    public static void main(String[] args) {
 	System.out.println("hello world! wangyue");
-	System.out.println("hello world! wangyue");
-	System.out.println("hello world! wangyue");
-	System.out.println("hello world! wangyue");
-	System.out.println("hello world! wangyue");
 }
 }   
